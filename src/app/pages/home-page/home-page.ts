19f3585bfdd66346deab19a70c3ab9c1eac3e9e8
Latouche Gerson <latouche822@gmail.com>
2025-10-16 15:08:12 -0400

    up120

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Navbar } from "../../components/navbar/navbar";
 import { Footer } from "../../components/footer/footer";
 import { MyButton } from "../../components/my-button/my-button";
@@ -10,10 +10,32 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angul
 import { SelectType } from '../../models/SelectType';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { Carousel } from 'primeng/carousel';
+import { Tag } from 'primeng/tag';
+import { CommonModule } from '@angular/common';
+
+interface Product {
+    id: string,
+    code: string,
+    name: string,
+    description: string,
+    image: string,
+    price: number,
+    category: string,
+    quantity: number,
+    inventoryStatus: string,
+    rating: number
+}
+
+interface CardService {
+  name: string;
+  description: string;
+  cover: string;
+}
 
 @Component({
   selector: 'app-home-page',
-  imports: [ButtonModule, CardModule, Navbar, Footer, ReactiveFormsModule, MySlider, MyHero, Select],
+  imports: [Carousel, CommonModule, ButtonModule, CardModule, Navbar, Footer, ReactiveFormsModule, MySlider, MyHero, Select, Tag],
   templateUrl: './home-page.html',
   styles: `
   .hero-section{
@@ -74,7 +96,58 @@ import { ButtonModule } from 'primeng/button';
 }
   `
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  products: Product[] = [
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5
+    }
+  ]
+
+  cardServices: CardService[] = [
+    {
+      name: 'Building Maintenance',
+      description: 'Protecting your investment requires proactive and expert maintenance. We keep your infrastructure running smoothly and efficiently...',
+      cover: 'img/services/new/building_maintenance.jpg'
+    },
+    {
+      name: 'Security',
+      description: 'Protecting your assets, people, and property is our paramount commitment. Our security experts provide a powerful blend of technology and human presence...',
+      cover: 'img/services/security_guard.jpg'
+    },
+    {
+      name: 'Housekeeping',
+      description: 'Beyond simply cleaning, our services ensure a healthy, productive, and welcoming environment for your employees and clients...',
+      cover: 'img/services/housekeeping1.jpg'
+    },
+    {
+      name: 'Janitorial',
+      description: 'Deep cleaning, floor care (stripping, waxing, buffing), window cleaning, and waste management...',
+      cover: 'img/services/new/janitorial_hospital.jpg'
+    },
+    {
+      name: 'Staffing',
+      description: 'When you need specialized help fast, our staffing division provides vetted, trained, and reliable personnel to fill essential roles within your facility or organization...',
+      cover: 'img/services/staffing_solution.jpg'
+    },
+    {
+      name: 'Concierges',
+      description: 'Elevate the experience for residents, tenants, and visitors with professional, hospitable, and highly effective concierge support...',
+      cover: 'img/services/concierge.jpg'
+    }
+  ]
+
+  responsiveOptions: any[] | undefined;
+
   services: SelectType[] = [
     { name: 'Security', code: 'SS' },
     { name: 'Housekeeping', code: 'HK' },
@@ -90,4 +163,29 @@ export class HomePage {
   formGroup = new FormGroup({
     selectedService: new FormControl(this.selectedService)
   })
+
+  ngOnInit(): void {
+     this.responsiveOptions = [
+            {
+                breakpoint: '1400px',
+                numVisible: 2,
+                numScroll: 1
+            },
+            {
+                breakpoint: '1199px',
+                numVisible: 3,
+                numScroll: 1
+            },
+            {
+                breakpoint: '767px',
+                numVisible: 2,
+                numScroll: 1
+            },
+            {
+                breakpoint: '575px',
+                numVisible: 1,
+                numScroll: 1
+            }
+        ]
+  }
 }
