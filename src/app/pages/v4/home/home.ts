c64c2a0db533455d42d2351e0c1555d1ed4a71ec
GoldyRoger07 <latouche822@gmail.com>
2026-05-19 16:56:01 -0400

    u123ruru

--- src/app/pages/v4/home/home.ts
+++ src/app/pages/v4/home/home.ts
@@ -17,17 +17,18 @@ export default class Home implements OnInit, OnDestroy {
   fade = signal(false)
 
   images = [
+    "v4/img/hero_section.jpg",
     "img/services/new/compressed/facility_management_1_1440.webp",
     "img/services/new/compressed/construction_worker_and_maintenance_1440.webp",
-    "img/services/new/compressed/facility_management_1440.webp",
-    "img/services/new/compressed/facility_manager_1440.webp",
-    "img/services/compressed/concierge_1440.webp",
+    "img/services/new/compressed/facility_management_1440.webp"
+    // "img/services/new/compressed/facility_manager_1440.webp",
+    // "img/services/compressed/concierge_1440.webp",
 
-    "img/services/new/compressed/housekeeping_team_1440.webp",
-    "img/services/new/compressed/janitorial_hospital_1440.webp",
-    "img/services/new/compressed/janitorial_1440.webp",
-    "img/services/compressed/security_guard_1440.webp",
-    "img/services/compressed/staffing_solution_1440.webp"
+    // "img/services/new/compressed/housekeeping_team_1440.webp",
+    // "img/services/new/compressed/janitorial_hospital_1440.webp",
+    // "img/services/new/compressed/janitorial_1440.webp",
+    // "img/services/compressed/security_guard_1440.webp",
+    // "img/services/compressed/staffing_solution_1440.webp"
   ]
   ngOnInit(): void {
     this.interval = setInterval((e:any)=>{
